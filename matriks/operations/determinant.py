3d80b58eb07f3867890d029ddaa0541ba922076a
Christine0128 <christineaulin28@gmail.com>
2025-10-17 16:48:58 +0700

    Menambahkan Matriks

---
+++ matriks/operations/determinant.py
@@ -0,0 +1,6 @@
+from matriks.matrix import Matrix
+from matriks.utilities import find_determinant
+
+def determinant(matrix):
+     """Mengembalikan nilai determinan matriks."""
+     return find_determinant(matrix)
