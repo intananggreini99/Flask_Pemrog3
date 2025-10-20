f62b8acb0649de0be26b1c833e5726cda37a00e8
intananggreini99 <d.intananggreini99@gmail.com>
2025-10-20 13:54:57 +0700

    Delete matriks/operations/determinant.py

--- matriks/operations/determinant.py
+++
@@ -1,6 +0,0 @@
-from matriks.matrix import Matrix
-from matriks.utilities import find_determinant
-
-def determinant(matrix):
-     """Mengembalikan nilai determinan matriks."""
-     return find_determinant(matrix)
